c245103a9b1d817d5d3d7b3b775c0e4ad70b9c8b
Bhavesh Sooka <bsooka@gmail.com>
2021-07-02 09:00:25 +0200

    Formatting

--- euler-problems/src/main/java/za/co/projects/eulerproblems/EulerProblemsApplication.java
+++ euler-problems/src/main/java/za/co/projects/eulerproblems/EulerProblemsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EulerProblemsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EulerProblemsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EulerProblemsApplication.class, args);
+    }
 
 }
